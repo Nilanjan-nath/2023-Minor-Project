e46bee2a111a9da88f7fe0db263bea8b60253f86
Nilanjan-nath <131948420+Nilanjan-nath@users.noreply.github.com>
2023-10-13 16:20:53 +0530

    changes: index.html and app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template , request , url_for, blueprints , redirect
+from flask import Flask , render_template , request , url_for, blueprints , redirect ,flash
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -26,7 +26,7 @@ with app.app_context():
 
 
 @app.route('/')
-def helloWorld():
+def home():
     user_list = PasswordManager.query.all()
     return render_template("index.html", user_list = user_list)
 
@@ -40,21 +40,40 @@ def add_details():
         user = PasswordManager(email = email, site_password = site_password, site_name = site_name)
         db.session.add(user)
         db.session.commit()
-        return redirect(url_for("/"))
+        return redirect(url_for("home"))
 
 
         
    
 
 
-@app.route('/update')
-def update_details():
-    return "Hello world"
+@app.route('/update/<int:id>',methods = ["GET", "POST"])
+def update_details(id):
+    updated_details = PasswordManager.query.filter_by(id=id).first()
+    if request.method=="POST":
+       
+            updated_details.email =request.form["email"]
+            updated_details.site_password =request.form["site_password"]
+            updated_details.site_name =request.form["site_name"]
+            db.session.commit()
+            return redirect(url_for("home"))
+    return render_template("update.html", updated_details = updated_details)
 
 
-@app.route('/delete')
-def delete_details():
-    return "Hello world"
+@app.route('/delete/<int:id>', methods=["GET","POST"])
+def delete_details(id):
+    details_to_delete =PasswordManager.query.filter_by(id=id).first()
+   
+    if details_to_delete:
+            db.session.delete(details_to_delete)
+            db.session.commit()
+            flash("Site deleted successfully",category="success")
+            return redirect(url_for("home"))
+
+    else:
+        flash("Some error has occurred")
+        return render_template("index.html")
+        
 
 
 
