191901ad517c057d86813a9b9ec58b3257bee579
Nilanjan-nath <131948420+Nilanjan-nath@users.noreply.github.com>
2023-10-16 13:40:22 +0530

    code: added init file

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,63 @@
-from flask import Flask
-from flask import Blueprint
+from flask import Flask, render_template, url_for,redirect, request, flash
+
+from .extensions import db,login_manager
+from .models import PasswordManager, User
+from flask_login import login_required
+
 def createapp():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'ASDASDFLKJADSF LKJKL'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ppmanager2.db'
+    app.config['SECRET_KEY'] = 'mysecretkey'
+    db.init_app(app)
 
-    from .views import views
-    app.register_blueprint(views, url_prefix=("/"))
+    with app.app_context():
+         db.create_all()
 
+    @app.route('/')
+    def home():
+        user_list = PasswordManager.query.all()
+        return render_template("index.html", user_list = user_list)
+
+    @app.route('/add', methods =["GET", "POST"])
+
+    def add_details():
+        if request.method == "POST":
+            email = request.form["email"]
+            site_password = request.form["site_password"]
+            site_name = request.form["site_name"]
+            user = PasswordManager(email = email, site_password = site_password, site_name = site_name)
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for("home"))
+        
+
+    
+    @app.route('/update/<int:id>',methods = ["GET", "POST"])
+    def update_details(id):
+        updated_details = PasswordManager.query.filter_by(id=id).first()
+        if request.method=="POST":
+        
+                updated_details.email =request.form["email"]
+                updated_details.site_password =request.form["site_password"]
+                updated_details.site_name =request.form["site_name"]
+                db.session.commit()
+                return redirect(url_for("home"))
+        return render_template("update.html", updated_details = updated_details)
+
+
+    @app.route('/delete/<int:id>', methods=["GET","POST"])
+    def delete_details(id):
+        details_to_delete =PasswordManager.query.filter_by(id=id).first()
+    
+        if details_to_delete:
+                db.session.delete(details_to_delete)
+                db.session.commit()
+                flash("Site deleted successfully",category="success")
+                return redirect(url_for("home"))
+
+        else:
+            flash("Some error has occurred")
+            return render_template("index.html")
+        
+    
     return app
---
+++ website/models.py
@@ -0,0 +1,23 @@
+from .extensions import db
+from flask_login import UserMixin
+
+#model
+
+class PasswordManager(db.Model):
+    __tablename__ = 'PasswordManager'
+    id = db.Column(db.Integer, primary_key=True)
+    email= db.Column(db.String(20), unique=False, nullable=False )
+    site_password = db.Column(db.String(20),unique=False,nullable=False)
+    site_name = db.Column(db.String(220), nullable=False)
+
+    def __repr__(self):
+        return '<PasswordManager %r>' %self.email
+    
+class User(UserMixin , db.Model):
+    id = db.Column(db.Integer , primary_key = True)
+    name = db.Column(db.String(250))
+    email = db.Column(db.String(220), nullable = False)
+    password = db.Column(db.String(64),nullable = False)
+
+    def __repr__(self):
+        return '<User %r>' % self.email
